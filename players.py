2f4173382d4c839482e0635b6e6e33ed32a7e6fc
Damwid99 <damwid@live.com>
2023-10-22 18:10:15 +0200

    players.py searching for full names

--- players.py
+++ players.py
@@ -14,23 +14,39 @@ def search_for_players(ending_of_url, team1, team2, team_for_project):
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         sections = soup.find_all('table', class_='taula_estil')
-        
+
         for section_number in range(0,4):
             section=sections[section_number]
             rows = section.find_all('tr')[1:] 
 
             for row in rows:
                 columns = row.find_all('td')
-                player_name = columns[3].text
+                players_pseudonim = columns[3].text
 
-                if player_name!='' and team1==team_for_project:
+                if players_pseudonim!='' and team1==team_for_project:
                     if section_number==0 or section_number==2:
-                        players.append(player_name)
 
-                elif player_name!='' and team2==team_for_project:
+                        link = columns[3].find('a')  # Find the anchor tag in the column
+                        player_link = link.get('href')  # Get the href attribute of the anchor tag
+                        url_for_one_player = f"https://www.bdfutbol.com/en/{player_link}"  # Construct the complete URL
+                        response_for_players_name = requests.get(url_for_one_player)
+                        soup_for_players = BeautifulSoup(response_for_players_name.text, 'html.parser')
+                        sections_for_player = soup_for_players.find_all('title')
+                        players_name = sections_for_player[0].text[sections_for_player[0].text.find(',')+2:sections_for_player[0].text.find(' -')]
+                        players.append(players_name)
+
+                elif players_pseudonim!='' and team2==team_for_project:
                     if section_number==1 or section_number==3:
-                        players.append(player_name)
 
+                        link = columns[3].find('a')  # Find the anchor tag in the column
+                        player_link = link.get('href')  # Get the href attribute of the anchor tag
+                        url_for_one_player = f"https://www.bdfutbol.com/en/{player_link}"  # Construct the complete URL
+                        response_for_players_name = requests.get(url_for_one_player)
+                        soup_for_players = BeautifulSoup(response_for_players_name.text, 'html.parser')
+                        sections_for_player = soup_for_players.find_all('title')
+                        players_name = sections_for_player[0].text[sections_for_player[0].text.find(',')+2:sections_for_player[0].text.find(' -')]
+
+                        players.append(players_name)
     return players
 
 
@@ -58,7 +74,6 @@ def save_to_excel(name_of_team, name_of_excel, matches_list):
                 sheet.cell(row=workbook_row, column=1, value=0)
 
             players=search_for_players(list_row[8], list_row[5], list_row[9], name_of_team)
-
             sheet.cell(row=workbook_row, column=2, value=list_row[0])
             sheet.cell(row=workbook_row, column=3, value=list_row[5])
             sheet.cell(row=workbook_row, column=4, value=list_row[9])
@@ -69,12 +84,12 @@ def save_to_excel(name_of_team, name_of_excel, matches_list):
                 
             workbook_row+=1
         except:
-            worbook.save(f'{name_of_excel}.csv')
+            worbook.save(f'./Football_team_evolution/{name_of_excel}.csv')
 
             worbook.close()
             quit()
 
-    worbook.save(f'{name_of_excel}.csv')
+    worbook.save(f'.//Football_team_evolution/{name_of_excel}.csv')
     worbook.close()
 
 
